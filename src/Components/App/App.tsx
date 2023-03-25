14e4eae0eb4a8e75b7d0db3773d6b7124fb17126
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-25 14:02:55 +0200

    update

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Footer } from '../Footer';
+import { Header } from '../Header';
 import './App.scss';
 
 export const App = () => {
   return (
     <div className="App">
-      hi
+      <Header />
+
+      <Footer />
     </div>
   );
 }
