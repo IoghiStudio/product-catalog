9efa5464be1457eb3263b0215d935e0852bab80f
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-26 19:46:31 +0300

    update

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { Logo } from '../Logo';
 import './Footer.scss';
 
+const footerLinks = [
+  {
+    url: '/',
+    name: 'Github'
+  },
+  {
+    url: '/',
+    name: 'Contacts'
+  },
+  {
+    url: '/',
+    name: 'Rights'
+  },
+]
+
 export const Footer = () => {
   return (
     <div className='footer'>
@@ -10,7 +25,20 @@ export const Footer = () => {
       />
 
       <ul className='footer__list'>
+        {footerLinks.map(link => {
+          const { url, name} = link;
 
+          return (
+            <li className='footer__item'>
+              <a
+                href={url}
+                className='footer__link'
+              >
+                {name}
+              </a>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
