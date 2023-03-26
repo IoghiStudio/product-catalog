3ca8ba2798e45211b2a629b06cb909cff95068f3
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-26 23:10:59 +0300

    update

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,12 +1,73 @@
 import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
+import { Routes, Route, Navigate} from 'react-router-dom';
 import './App.scss';
+import { HomePage } from '../HomePage';
+import { PhonesPage } from '../PhonesPage';
+import { TabletsPage } from '../TabletsPage';
+import { AccessoriesPage } from '../AccessoriesPage';
+import { CartPage } from '../CartPage';
+import { FavoritesPage } from '../FavoritesPage';
 
 export const App = () => {
   return (
     <div id='top' className="App">
       <Header />
+
+      <Routes>
+        <Route
+          element={<HomePage />}
+          path="/"
+        />
+
+        <Route
+          path="/home"
+          element={
+            <Navigate to="/" replace/>
+          }
+        />
+
+        <Route
+          path="/phones"
+          element={
+            <PhonesPage />
+          }
+        />
+
+        <Route
+          path="/tablets"
+          element={
+            <TabletsPage />
+          }
+        />
+
+        <Route
+          path="/accessories"
+          element={
+            <AccessoriesPage />
+          }
+        />
+
+        <Route
+          path="/favorites"
+          element={
+            <FavoritesPage />
+          }
+        />
+
+        <Route
+          path="/cart"
+          element={
+            <CartPage />
+          }
+        />
+
+        <Route
+          element={'404 page not found'}
+          path='*'
+        />
+      </Routes>
 
       <Footer />
     </div>
--- src/Components/TabletsPage/Tablets.tsx
+++ src/Components/TabletsPage/Tablets.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const TabletsPage = () => {
   return (
-    <div>**</div>
+    <div>Tablets</div>
   )
 }
