257c8cf23740a3293c4fe3988680a9e8ac5689c8
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-27 19:16:46 +0300

    update

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -29,29 +29,33 @@ type Props = {
 export const ProductCard = () => {
   return (
     <div className="productcard">
-      <div className="productcard__image"></div>
+      <div className="productcard__image">
+        wesfewf
+      </div>
 
-      <div className="productcard__title"></div>
+      <div className="productcard__title">
+        Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
+      </div>
 
       <div className="productcard__prices">
-        <div className="productcard__price"></div>
-        <div className="productcard__fullprice"></div>
+        <div className="productcard__price">$799</div>
+        <div className="productcard__fullprice">$899</div>
       </div>
 
       <div className="productcard__description">
-        <div className="productcard__details">
-          <div className="productcard__details--text">Screen</div>
-          <div className="productcard__details--value"></div>
+        <div className="productcard__specs">
+          <div className="productcard__specs--text">Screen</div>
+          <div className="productcard__specs--value">5.7led</div>
         </div>
 
-        <div className="productcard__details">
-          <div className="productcard__details--text">Capacity</div>
-          <div className="productcard__details--value"></div>
+        <div className="productcard__specs">
+          <div className="productcard__specs--text">Capacity</div>
+          <div className="productcard__specs--value">64 gb</div>
         </div>
 
-        <div className="productcard__details">
-          <div className="productcard__details--text">RAM</div>
-          <div className="productcard__details--value"></div>
+        <div className="productcard__specs">
+          <div className="productcard__specs--text">RAM</div>
+          <div className="productcard__specs--value">4 gb</div>
         </div>
       </div>
 
