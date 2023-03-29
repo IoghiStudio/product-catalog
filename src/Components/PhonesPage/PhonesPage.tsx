2d605389a6c867cfcbb4e723e3ebfe1ba15e3beb
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-29 16:31:08 +0300

    update

--- src/Components/PhonesPage/PhonesPage.tsx
+++ src/Components/PhonesPage/PhonesPage.tsx
@@ -2,25 +2,53 @@ import React, { useState, useEffect } from 'react';
 import phonesFromServer from '../../client/api/phones.json';
 import { Phone } from '../../types/phone';
 import { ProductList } from '../ProductList';
+import './PhonesPage.scss';
+
+enum FilterBy {
+  All = 'all',
+  Alph = 'alph',
+  Newest = 'newest',
+  Cheapest = 'cheapest'
+}
 
 
 export const PhonesPage = () => {
   const [phones, setPhones] = useState<Phone[]>([]);
-  // const [visiblePhones, setViziblePhones] = useState<Phone[]>([]); 
+  const [visiblePhones, setViziblePhones] = useState<Phone[]>([]); 
+  const [filterBy, setFilterBy] = useState<FilterBy>(FilterBy.All);
 
   useEffect(() => {
     setPhones(phonesFromServer);
   }, [])
 
+  useEffect(() => {
+    let sortedPhones: Phone[];
+
+    switch(filterBy) {
+      case FilterBy.Alph:
+        sortedPhones = [...phones].sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case FilterBy.Newest:
+        sortedPhones = [...phones].sort((a, b) => b.year - a.year);
+        break;
+      case FilterBy.Cheapest:
+        sortedPhones = [...phones].sort((a, b) => a.price - b.price);
+        break;
+      default:
+        sortedPhones = [...phones];
+    }
+
+
+    setViziblePhones(sortedPhones);
+  }, [phones, filterBy])
+
 
   return (
     <div className='phones'>
-      <div>Mobile phones</div>
-
-      <h1>Phones available: </h1>
+      <h1 className='phones__title'>Mobile phones</h1>
 
       <ProductList
-        products={phones}
+        products={visiblePhones}
       />
     </div>
   )
