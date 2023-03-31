b347826e5b87f46902efe1165aefb99f8e5f994d
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-31 15:23:53 +0300

    update

--- src/Components/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/Components/ProductDetailsPage/ProductDetailsPage.tsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useMatch } from "react-router-dom";
 import './ProductDetailsPage.scss';
 import phoneFromServer from '../../client/api/phones/apple-iphone-11-128gb-purple.json';
+import { PhoneDetails } from '../../types/phoneDetails';
 
 
 export const ProductDetailsPage = () => {
-  const [phone, setPhone] = useState([]);
-  const [phonePath, setPhonePath] = useState([]);
+  const [phone, setPhone] = useState<PhoneDetails | []>([]);
+  // const [phonePath, setPhonePath] = useState([]);
 
   const match = useMatch('/phones/:phoneId');
 
