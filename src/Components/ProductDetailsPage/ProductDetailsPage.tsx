a026fe135c50e788bf2856751ec06af94aff57e6
Nicusor Iorga <ioghigithublab@gmail.com>
2023-04-01 19:37:15 +0300

    update

--- src/Components/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/Components/ProductDetailsPage/ProductDetailsPage.tsx
@@ -23,17 +23,19 @@ export const ProductDetailsPage = () => {
           {phone.name}
         </h2>
   
-      <div className="details__images">
-        <img
-            className="details__images--image"
-            src={require(`../../client/api/${phone.images[0]}`)}
-            alt={phone.name}
-          />
+        <div className="details__top">
+          <div className="details__images">
+            <img
+                className="details__images--image"
+                src={require(`../../client/api/${phone.images[0]}`)}
+                alt={phone.name}
+              />
 
-        <div className="details__images--previews">
-          
+            <div className="details__images--previews">
+              
+            </div>
+          </div>
         </div>
-      </div>
       </div>
     )
   } else {
