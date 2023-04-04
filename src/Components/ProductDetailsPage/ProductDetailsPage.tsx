945dcba698534fb44020bfd6d08bee60bfd7607d
Nicusor Iorga <ioghigithublab@gmail.com>
2023-04-04 21:31:06 +0300

    update

--- src/Components/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/Components/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from "react-router-dom";
 import './ProductDetailsPage.scss';
-import phoneFromServer from '../../client/api/phones/apple-iphone-11-128gb-red.json'
+// import phoneFromServer from '../../client/api/phones/apple-iphone-11-128gb-red.json'
 import { PhoneDetails } from '../../types/phoneDetails';
 import { NotFoundPage } from '../NotFoundPage';
 import cn from 'classnames';
@@ -16,22 +16,29 @@ export const ProductDetailsPage = () => {
   const [capacityAvailable, setCapacityAvailable] = useState<string[]>([]);
   const [currentImage, setCurrentImage] = useState<string>('');
   const [currentCapacity, setCurrentCapacity] = useState<string>('');
-  const obj22 = useParams();
+  const { phoneId } = useParams();
 
   // const [phonePath, setPhonePath] = useState([]);
   // const match = useMatch('/phones/:phoneId');
 
   useEffect(() => {
-    setPhone(phoneFromServer)
-    setColorsAvailable(phoneFromServer.colorsAvailable)
-    setImages(phoneFromServer.images);
-    setCurrentImage(phoneFromServer.images[0])
-    setCapacityAvailable(phoneFromServer.capacityAvailable)
-    setCurrentCapacity(phoneFromServer.capacity)
-    
+    fetch(`../../client/api/phones/${phoneId}.json`)
+      .then(resp => resp.json())
+      .then(data => console.log(data))
+
+    // fetch('../../client/api/phones/apple-iphone-11-128gb-black.json')
+    //   .then(resp => resp.json())
+
+    // setPhone(phoneFromServer)
+    // setColorsAvailable(phoneFromServer.colorsAvailable)
+    // setImages(phoneFromServer.images);
+    // setCurrentImage(phoneFromServer.images[0])
+    // setCapacityAvailable(phoneFromServer.capacityAvailable)
+    // setCurrentCapacity(phoneFromServer.capacity)
+      
   }, [])
 
-  console.log(obj22)
+  // console.log(phoneId)
 
   if (phone) {
     return (
