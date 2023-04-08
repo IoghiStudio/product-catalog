bdf848ee6b59ad3705b8997dc2e20ed53ba78a89
Nicusor Iorga <ioghigithublab@gmail.com>
2023-04-08 13:25:49 +0300

    update

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -61,7 +61,7 @@ export const HomePage = () => {
   );
 
   useEffect(() => {
-    fetch('./product-catalog/phones.json')
+    fetch('./phones.json')
       .then(resp => resp.json())
       .then(phones => {
         setPhones(phones);
@@ -70,15 +70,15 @@ export const HomePage = () => {
 
   let timeout: string | number | NodeJS.Timeout | undefined;
 
-  // useEffect(() => {
-  //   timeout = setTimeout(() => {
-  //     handleBannerChange(Way.Next, bannerIndex);
-  //   }, 3000);
+  useEffect(() => {
+    timeout = setTimeout(() => {
+      handleBannerChange(Way.Next, bannerIndex);
+    }, 3000);
     
-  //   return () => {
-  //     clearTimeout(timeout)
-  //   }
-  // }, [bannerIndex])
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [bannerIndex])
   
   return (
     <div className='home'>
