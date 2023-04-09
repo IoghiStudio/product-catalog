847662019d546a7b8f80a9529b19830a98e4b8d9
Nicusor Iorga <ioghigithublab@gmail.com>
2023-04-09 11:35:13 +0300

    update

--- src/Components/CartItem/CartItem.tsx
+++ src/Components/CartItem/CartItem.tsx
@@ -22,75 +22,79 @@ export const CartItem = () => {
   if (phone) {
     return (
       <div className="cartitem">
-        <div className="cartitem__remove"></div>
+        <div className="cartitem__top">
+          <div className="cartitem__remove"></div>
 
-        <Link
-          to={`/phones/${phone.phoneId}`}
-          className="cartitem__image-container"
-        >
-          <img
-            className="cartitem__image"
-            src={require(`../../api/${phone.image}`)}
-            alt={phone.name}
-          />
-        </Link>
-  
-        <div className="cartitem__name">
-          {phone.name}
-          {' '}
-          (iMT9G2FS/A)
-        </div>
-  
-        <div className="cartitem__icons">
-          <div
-            className={cn(
-              "cartitem__icon-container",
-              {
-                "cartitem__icon-container--disabled": count === 1,
-              }
-            )}
-            onClick={() => {
-              if (count > 1) {
-                setCount(count - 1);
-              }
-            }}
+          <Link
+            to={`/phones/${phone.phoneId}`}
+            className="cartitem__image-container"
           >
-            <div className={cn(
-              "cartitem__icon",
-              "cartitem__icon--minus",
-              {
-                "cartitem__icon--minus--disabled": count === 1,
-              }
-            )}></div>
-          </div>
-  
-          <div className="cartitem__count">{count}</div>
-            
-          <div
-            className={cn(
-              "cartitem__icon-container",
-              {
-                "cartitem__icon-container--disabled": count === 6,
-              }
-            )}
-            onClick={() => {
-              if (count < 6) {
-                setCount(count + 1);
-              }
-            }}
-          >
-            <div className={cn(
-              "cartitem__icon",
-              "cartitem__icon--plus",
-              {
-                "cartitem__icon--plus--disabled": count === 6,
-              }
-            )}></div>
+            <img
+              className="cartitem__image"
+              src={require(`../../api/${phone.image}`)}
+              alt={phone.name}
+            />
+          </Link>
+
+          <div className="cartitem__name">
+            {phone.name}
+            {' '}
+            (iMT9G2FS/A)
           </div>
         </div>
   
-        <div className="cartitem__price">
-          {`$${999}`}
+        <div className="cartitem__bottom">
+          <div className="cartitem__icons">
+            <div
+              className={cn(
+                "cartitem__icon-container",
+                {
+                  "cartitem__icon-container--disabled": count === 1,
+                }
+              )}
+              onClick={() => {
+                if (count > 1) {
+                  setCount(count - 1);
+                }
+              }}
+            >
+              <div className={cn(
+                "cartitem__icon",
+                "cartitem__icon--minus",
+                {
+                  "cartitem__icon--minus--disabled": count === 1,
+                }
+              )}></div>
+            </div>
+    
+            <div className="cartitem__count">{count}</div>
+              
+            <div
+              className={cn(
+                "cartitem__icon-container",
+                {
+                  "cartitem__icon-container--disabled": count === 6,
+                }
+              )}
+              onClick={() => {
+                if (count < 6) {
+                  setCount(count + 1);
+                }
+              }}
+            >
+              <div className={cn(
+                "cartitem__icon",
+                "cartitem__icon--plus",
+                {
+                  "cartitem__icon--plus--disabled": count === 6,
+                }
+              )}></div>
+            </div>
+          </div>
+    
+          <div className="cartitem__price">
+            {`$${999}`}
+          </div>
         </div>
       </div>
     )
