d1843ae20e2066f11fe411877bc3602ec51f8856
Nicusor Iorga <ioghigithublab@gmail.com>
2023-04-09 11:30:55 +0300

    update

--- src/Components/CartItem/CartItem.tsx
+++ src/Components/CartItem/CartItem.tsx
@@ -1,59 +1,102 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CartItem.scss';
 import cn from 'classnames';
+import { Phone } from '../../types/phone';
+import { NotFoundPage } from '../NotFoundPage';
+import { Link } from 'react-router-dom';
 
 type Props = {
 
 }
 
-export const CartItem: React.FC<Props> = () => {
-  const [count, setCount] = useState(0);
+export const CartItem = () => {
+  const [count, setCount] = useState(1);
+  const [phone, setPhone] = useState<Phone | null>(null);
 
-  return (
-    <div className="cartitem">
-      <div className="cartitem__icons">
-        <div
-          className={cn(
-            "cartitem__icon-container",
-            {
-              "cartitem__icon-container--disabled": count === 0,
-            }
-          )}
-          onClick={() => setCount(count - 1)}
+  useEffect(() => {
+    fetch('./product-catalog/phones.json')
+      .then(response => response.json())
+      .then(data => setPhone(data[82]));
+  }, [])
+
+  if (phone) {
+    return (
+      <div className="cartitem">
+        <div className="cartitem__remove"></div>
+
+        <Link
+          to={`/phones/${phone.phoneId}`}
+          className="cartitem__image-container"
         >
-          <div className={cn(
-            "cartitem__icon",
-            "cartitem__icon--minus",
-            {
-              "cartitem__icon--minus--disabled": count === 0,
-            }
-          )}></div>
+          <img
+            className="cartitem__image"
+            src={require(`../../api/${phone.image}`)}
+            alt={phone.name}
+          />
+        </Link>
+  
+        <div className="cartitem__name">
+          {phone.name}
+          {' '}
+          (iMT9G2FS/A)
         </div>
-
-        <div className="cartitem__count">{count}</div>
-          
-        <div
-          className={cn(
-            "cartitem__icon-container",
-            {
-              "cartitem__icon-container--disabled": count === 4,
-            }
-          )}
-          onClick={() => setCount(count + 1)}
-        >
-          <div className={cn(
-            "cartitem__icon",
-            "cartitem__icon--plus",
-            {
-              "cartitem__icon--plus--disabled": count === 4,
-            }
-          )}></div>
+  
+        <div className="cartitem__icons">
+          <div
+            className={cn(
+              "cartitem__icon-container",
+              {
+                "cartitem__icon-container--disabled": count === 1,
+              }
+            )}
+            onClick={() => {
+              if (count > 1) {
+                setCount(count - 1);
+              }
+            }}
+          >
+            <div className={cn(
+              "cartitem__icon",
+              "cartitem__icon--minus",
+              {
+                "cartitem__icon--minus--disabled": count === 1,
+              }
+            )}></div>
+          </div>
+  
+          <div className="cartitem__count">{count}</div>
+            
+          <div
+            className={cn(
+              "cartitem__icon-container",
+              {
+                "cartitem__icon-container--disabled": count === 6,
+              }
+            )}
+            onClick={() => {
+              if (count < 6) {
+                setCount(count + 1);
+              }
+            }}
+          >
+            <div className={cn(
+              "cartitem__icon",
+              "cartitem__icon--plus",
+              {
+                "cartitem__icon--plus--disabled": count === 6,
+              }
+            )}></div>
+          </div>
+        </div>
+  
+        <div className="cartitem__price">
+          {`$${999}`}
         </div>
       </div>
-
-      <div className="cartitem__price">
-        {`$${999}`}
-      </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <NotFoundPage />
+    )
+  }
 }
